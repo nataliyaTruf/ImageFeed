4b293b02d86147353fef4863ce1a0540d1e3ef50
nataliyaTruf <nataliya.truffez@gmail.com>
2023-07-29 20:48:42 +0200

    Экспортировала цвета из макета Figma Создала папку Helpers и файл с перечнем статических названий цветов UIColor+Extensions.swift

---
+++ ImageFeed/Helpers/UIColor+Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  UIColor+Extensions.swift
+//  ImageFeed
+//
+//  Created by Nataliya MASSOL on 29/07/2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static var ypBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black }
+    static var ypGray: UIColor { UIColor(named: "YP Gray") ?? UIColor.gray }
+    static var ypBackground: UIColor { UIColor(named: "YP Background") ?? UIColor.darkGray }
+    static var ypWhite: UIColor { UIColor(named: "YP White") ?? UIColor.white }
+    static var ypWhiteAlpha: UIColor { UIColor(named: "YP White (Alpha 50)") ?? UIColor.white.withAlphaComponent(0.5) }
+    static var ypRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red }
+    static var ypBlue: UIColor { UIColor(named: "YP Blue") ?? UIColor.blue }
+}
+
