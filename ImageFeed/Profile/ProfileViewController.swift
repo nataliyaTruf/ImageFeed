6a5f007c19ea39b9022473b349a24bc526c8bf5a
nataliyaTruf <nataliya.truffez@gmail.com>
2023-08-19 21:09:17 +0300

    Сверстан экран профиля Создан класс ProfileViewController Загружены ресурсы из Figma: картинки для профиля и для кнопки логаута, подставлены в нужные элементы в Storyboard Созданы соответствующие @IBOutlet' ы и @IBAction

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Nataliya MASSOL on 18/08/2023.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet private var avatarImageView: UIImageView!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginNameLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+    
+    @IBAction private func didTouchLogoutButton() {
+    }
+    
+}
