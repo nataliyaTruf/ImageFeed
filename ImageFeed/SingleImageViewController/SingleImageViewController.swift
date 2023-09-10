6cb29be98fb8891818917146b1afa8ba4a387bff
nataliyaTruf <nataliya.truffez@gmail.com>
2023-09-10 16:05:17 +0200

    Создан экран для показа одной картинки SingleImageViewController Coздан файл SingleImageViewController c необходимыми настройками

---
+++ ImageFeed/SingleImageViewController/SingleImageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Nataliya MASSOL on 10/09/2023.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+}
