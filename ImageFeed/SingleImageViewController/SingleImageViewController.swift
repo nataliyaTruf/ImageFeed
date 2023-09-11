aa1c37cacb9600667039321a465d420e26d3ecee
nataliyaTruf <nataliya.truffez@gmail.com>
2023-09-11 11:53:43 +0200

    Добавлена кнопка и метод для скрытия модального окна

--- ImageFeed/SingleImageViewController/SingleImageViewController.swift
+++ ImageFeed/SingleImageViewController/SingleImageViewController.swift
@@ -21,4 +21,8 @@ final class SingleImageViewController: UIViewController {
         super.viewDidLoad()
         imageView.image = image
     }
+    
+    @IBAction func didTapBackButton() {
+        dismiss(animated: true, completion: nil)
+    }
 }
