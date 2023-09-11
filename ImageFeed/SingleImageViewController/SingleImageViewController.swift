1684e0d27df0cacd5992d93006bf27bb567aa2bf
nataliyaTruf <nataliya.truffez@gmail.com>
2023-09-11 16:09:33 +0200

    Реализован зум картинки экрана SingleImageViewController

--- ImageFeed/SingleImageViewController/SingleImageViewController.swift
+++ ImageFeed/SingleImageViewController/SingleImageViewController.swift
@@ -15,14 +15,25 @@ final class SingleImageViewController: UIViewController {
         }
     }
     
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+        
         imageView.image = image
     }
     
     @IBAction func didTapBackButton() {
         dismiss(animated: true, completion: nil)
+    }
+}
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        imageView
     }
 }
