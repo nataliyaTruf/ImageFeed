e8409e5dcef1014302d2bcb76b49738116856d1e
nataliyaTruf <nataliya.truffez@gmail.com>
2023-09-17 18:18:39 +0200

    Создан файл Constants

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Nataliya MASSOL on 17/09/2023.
+//
+
+import Foundation
+
+let AccessKey = "YIdDeipnqmyFrGPIPRGkH_xyqPr1WoZTsiBUG24Ug7c"
+let SecretKey = "35jhiuXM_bBuBfVw4WstuCw9TPNLFw88vYx5FCj8p1Q"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_like"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
