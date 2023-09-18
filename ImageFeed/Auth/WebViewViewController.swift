3e12542d20d0506e4de5d0d02bf235af39bb948c
nataliyaTruf <nataliya.truffez@gmail.com>
2023-09-18 14:50:34 +0200

    Создан экран WebViewViewController добавлены аутлеты

---
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewViewController.swift
+//  ImageFeed
+//
+//  Created by Nataliya MASSOL on 18/09/2023.
+//
+
+import WebKit
+
+final class WebViewViewController: UIViewController {
+    @IBOutlet var webView: WKWebView!
+ 
+    
+    @IBAction func didTapBackButton(_ sender: Any) {
+    }
+}
+
