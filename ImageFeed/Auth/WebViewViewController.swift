22cc608c130c06bed15ebfd442790a5221dd8690
nataliyaTruf <nataliya.truffez@gmail.com>
2023-09-18 15:59:19 +0200

    Сформирован URLRequest для загрузки экрана авторизации WebViewViewController

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -5,11 +5,30 @@
 //  Created by Nataliya MASSOL on 18/09/2023.
 //
 
+import UIKit
 import WebKit
 
+fileprivate let UnsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+
 final class WebViewViewController: UIViewController {
+    
     @IBOutlet var webView: WKWebView!
- 
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var urlComponents = URLComponents(string: UnsplashAuthorizeURLString)!
+        urlComponents.queryItems = [
+        URLQueryItem(name: "client_id", value: AccessKey),
+        URLQueryItem(name: "redirect_uri", value: RedirectURI),
+        URLQueryItem(name: "response_type", value: "code"),
+        URLQueryItem(name: "scope", value: AccessScope)
+        ]
+        let url = urlComponents.url!
+        
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
     
     @IBAction func didTapBackButton(_ sender: Any) {
     }
