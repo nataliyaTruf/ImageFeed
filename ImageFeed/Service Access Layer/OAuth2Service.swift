b04c6881b0b45d304425463e7fd148c986ccd73f
nataliyaTruf <nataliya.truffez@gmail.com>
2023-10-01 17:49:04 +0200

    Добавлена обработка для потенциальной гонки в fetchAuthToken()

--- ImageFeed/Service Access Layer/OAuth2Service.swift
+++ ImageFeed/Service Access Layer/OAuth2Service.swift
@@ -10,6 +10,8 @@ import Foundation
 final class OAuth2Service {
     static let shared = OAuth2Service()
     private let urlSession = URLSession.shared
+    private var task: URLSessionTask?
+    private var lastCode: String?
     private (set) var authToken: String? {
         get {
             return OAuth2TokenStorage().token
@@ -23,6 +25,10 @@ final class OAuth2Service {
         _ code: String,
         completion: @escaping (Result <String, Error>) -> Void
     ) {
+        assert(Thread.isMainThread)
+        if lastCode == code { return }
+        task?.cancel()
+        lastCode = code
         let request = authTokenRequest(code: code)
         let task = object(for: request) { [weak self] result in
             guard let self = self else { return }
@@ -31,10 +37,13 @@ final class OAuth2Service {
                 let authToken = body.accessToken
                 self.authToken = authToken
                 completion(.success(authToken))
+                self.task = nil
             case .failure(let error):
                 completion(.failure(error))
+                self.lastCode = nil
             }
         }
+        self.task = task
         task.resume()
     }
 }
