e1476b9e9489d82d4b7784c14d6d80f818d076cf
nataliyaTruf <nataliya.truffez@gmail.com>
2023-10-13 14:25:52 +0200

    Имплементирован в расширение URLSession дженерик-метод objectTask

--- ImageFeed/Network/URLSessionExtension.swift
+++ ImageFeed/Network/URLSessionExtension.swift
@@ -15,6 +15,19 @@ enum NetworkError: Error {
 }
 
 extension URLSession {
+    func objectTask<T: Decodable>(
+        for request: URLRequest,
+        completion: @escaping (Result <T, Error>) -> Void
+    ) -> URLSessionTask {
+        let decoder = SnakeCaseJSONDecoder()
+        return data(for: request) { (result: Result<Data, Error>) in
+            let response = result.flatMap { data -> Result<T, Error> in
+                Result { try decoder.decode(T.self, from: data) }
+            }
+            completion(response)
+        }
+    }
+    
     func data(
         for request: URLRequest,
         completion: @escaping (Result <Data, Error>) -> Void
